63404c2c40b57087a348c3fa6547da6859b881c5
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-02-26 12:27:43 -0600

    create driver and utility file

---
+++ OrderAutoGenerator/UtilityFunctions.py
@@ -0,0 +1,62 @@
+from datetime import date
+import random
+
+
+def incrementDate(curDate: date) -> date:
+    pass
+
+
+def getCustomerName() -> str:
+    pass
+
+
+def getEmployeeID() -> str:
+    pass
+
+
+def numOrdersForDay(gameDays: set) -> int:
+    pass
+
+
+def numItemsForOrder() -> int:
+    pass
+
+
+def selectSoldItems(weightedItems: dict, numItems: int) -> list:
+    pass
+
+
+def selectItem(weightedItems: dict) -> int:
+    pass
+
+
+def getItemPrice(menuItems: dict) -> float:
+    pass
+
+
+def createOrder(soldItems: list) -> list:
+    pass
+
+
+def createStringOfSoldItems(soldItems: list) -> str:
+    pass
+
+
+def createStringOfOrder(orderList: list) -> str:
+    pass
+
+
+def openMenu(fileName: str) -> dict:
+    pass
+
+
+def createMenuWeights(menuItems: dict) -> dict:
+    pass
+
+
+def isGameDay(date_to_check: date, gameDays: set) -> bool:
+    pass
+
+
+def parseGameDaysInput(days_string: str) -> set:
+    pass
