8b815c2e8dcd96db2fe00f396a69e976704c4c92
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-02-26 12:27:20 -0600

    dvide code into a driver and utility file

---
+++ OrderAutoGenerator/AutoGenerator.py
@@ -0,0 +1,26 @@
+from UtilityFunctions import *
+
+if __name__ == "main":
+    # get menu file
+
+    # convert menu file into a dict of menu weights
+
+    # get user input for start date and end date
+    # convert into date object
+
+    # get user input for game days
+    # parse the game days into a set
+
+    # open appropriate output files
+
+    # main loop for each day in the range
+    # - check if game day and generate a number of orders for that day
+    # - for each order
+    # -- generate a number of items for that order
+    # -- choose items for the order
+    # -- get prices for all those items and create an order with employee id and a customer name
+    # -- create a string for the order
+    # -- create a string for each item sold
+    # -- write the strings to the output files
+    # - increment date
+    pass
