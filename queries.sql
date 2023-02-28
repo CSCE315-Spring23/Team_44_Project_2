737dbeed4404fdc1a80f0a51b4176ba6464f7af4
Kevin Dai <kevindai@DESKTOP-I5GVE68.localdomain>
2023-02-28 16:58:31 -0600

    added more sql queries

--- queries.sql
+++ queries.sql
@@ -22,3 +22,35 @@ SELECT SUM(total_cost) FROM orderitem WHERE date='2022-09-03';
 /* average */
 SELECT AVG(total_cost) FROM orderitem WHERE customer_name='Victoria';
 SELECT AVG(total_cost) FROM orderitem WHERE date='2022-07-04';
+
+/* joining tables to process data */
+SELECT * FROM recipeitem 
+    JOIN inventory ON recipeitem.inventoryid = inventory.id 
+    JOIN menuitem ON recipeitem.menuid = menuitem.id 
+    WHERE menuitem.name = 'Chick-fil-A Chicken Sandwich'; /* given name */
+
+-- SELECT * from recipeitem 
+--     JOIN inventory ON recipeitem.inventoryid = inventory.id 
+--     JOIN menuitem ON recipeitem.menuid = menuitem.id 
+--     WHERE menuitem.name = 'Chick-fil-A Chicken Sandwich'; /* given id */
+
+/* specific employee */
+SELECT * FROM orderitem JOIN employee ON orderitem.employee_id = employee.pin::INT WHERE employee.name = 'Cluck Rogers' LIMIT 10; --for demonstration
+-- SELECT * FROM orderitem JOIN employee ON orderitem.employee_id = employee.id WHERE employee.name = 'Cluck Rogers' LIMIT 10; --after fix
+
+/* number sold for menu item */
+SELECT menuitem.name, COUNT(solditem.menuid) AS menuid_count
+    FROM solditem
+    JOIN menuitem ON solditem.menuid = menuitem.id
+    WHERE menuitem.name = 'M. Waffle Fries'
+    GROUP BY solditem.menuid, menuitem.name
+    ORDER BY menuid_count DESC
+    LIMIT 1;
+
+/* top 5 menu items */
+SELECT menuitem.name, COUNT(solditem.menuid) AS count
+    FROM solditem
+    JOIN menuitem ON solditem.menuid = menuitem.id
+    GROUP BY solditem.menuid, menuitem.name
+    ORDER BY count DESC
+    LIMIT 5;
