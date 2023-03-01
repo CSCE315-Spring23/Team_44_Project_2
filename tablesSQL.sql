520741768618d6537a69fed59dd01b0a21c8cab9
stevenmao27 <steven2003mao@gmail.com>
2023-02-28 19:05:45 -0600

    SQL Styling Convention

--- tablesSQL.sql
+++ tablesSQL.sql
@@ -1,40 +1,40 @@
-create table menuitem (
-id serial primary key,
-name text not null,
-cost numeric(6,2) not null,
-numbersold int not null
+CREATE TABLE menuitem (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+cost NUMERIC(6,2) NOT NULL,
+numbersold INT NOT NULL
 );
 
-create table inventory (
-id serial primary key,
-name text not null,
-quantity integer not null
+CREATE TABLE inventory (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+quantity INTEGER NOT NULL
 );
 
-create table orderitem(
-id serial primary key,
-customer_name text not null,
-total_cost numeric(10,2) not null,
-date timestamp default Current_timestamp,
-employee_id int not null
+CREATE TABLE orderitem(
+id SERIAL PRIMARY KEY,
+customer_name TEXT NOT NULL,
+total_cost NUMERIC(10,2) NOT NULL,
+date TIMESTAMP DEFAULT Current_timestamp,
+employee_id INT NOT NULL
 );
 
-create table solditem(
-id serial primary key,
-menuid int not null,
-orderid int not null
+CREATE TABLE solditem(
+id SERIAL PRIMARY KEY,
+menuid INT NOT NULL,
+orderid INT NOT NULL
 );
 
-create table recipeitem(
-id serial primary key,
-inventoryid int not null,
-menuid integer not null,
-count numeric(10,2) not null
+CREATE TABLE recipeitem(
+id SERIAL PRIMARY KEY,
+inventoryid INT NOT NULL,
+menuid INTEGER NOT NULL,
+count NUMERIC(10,2) NOT NULL
 );
 
-create table employees(
-id serial primary key,
-name text not null,
-role text not null,
-pin varchar(4) not null
+CREATE TABLE employees(
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+role TEXT NOT NULL,
+pin VARCHAR(4) NOT NULL
 );
