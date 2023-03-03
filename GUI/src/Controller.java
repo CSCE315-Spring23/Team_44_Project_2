56ea2076600686cd118ea89fd79046384e25408a
Kevin <kevin923.dai@gmail.com>
2023-03-03 10:19:35 -0600

    set up database connection

--- GUI/src/Controller.java
+++ GUI/src/Controller.java
@@ -1,7 +1,15 @@
+import java.sql.*;
+
+
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+
 public class Controller {
+
+    //database connection
+    private Connection conn;
+
 
     @FXML
     private Label label;
@@ -9,10 +17,49 @@ public class Controller {
     private TextArea textbox;
 
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        System.out.println("Initialized");
+        setUpDatabase();
+        textbox.setText(submitQuery("SElECT * FROM menuitem"));
+    }
 
-        textbox.setText("Hello World");
+
+    public void setUpDatabase(){
+        //database login info
+        String teamNumber = "team_44";
+        String dbName = "csce315331_" + teamNumber;
+        String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+        String username = "csce315331_team_44_master"; // change to your username
+        String password = "ShreemanLikesDeepWork"; //nothing to see here
+        
+        try{
+            conn = DriverManager.getConnection(dbConnectionString, username, password);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Error connecting to database");
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+
+    public String submitQuery(String query){
+        String ret = "";
+
+        System.out.println(query);
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next()){
+                ret += rs.getString("name") + "\n";
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Error accesing database");
+            System.exit(0);
+        }
+        System.out.println(ret);
+        return ret;
     }
 }
--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -8,14 +8,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Mainfx.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("server.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+        System.out.println("Hello World!");
     }
 }
