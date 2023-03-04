66c42431549405a9965685b645578b93619ffd00
Kevin <kevin923.dai@gmail.com>
2023-03-03 21:10:59 -0600

    started order logic

--- GUI/bin/compile.ps1
+++ GUI/bin/compile.ps1
@@ -1,4 +1,2 @@
-javac *.java --module-path ../lib --add-modules javafx.controls,javafx.fxml
-
-java --module-path C:\\Users\\'Kevin Dai'\\Downloads\\openjfx-19.0.2.1_windows-x64_bin-sdk\\javafx-sdk-19.0.2.1/lib --add-modules javafx.controls,javafx.fxml -cp ".;postgresql-42.2.8.jar" main.java
-
+javac *.java --module-path "C:\\Program Files\\Java\\javafx-sdk-19.0.2.1\\lib" --add-modules javafx.controls,javafx.fxml
+java --module-path "C:\\Program Files\\Java\\javafx-sdk-19.0.2.1\\lib" --add-modules javafx.controls,javafx.fxml -cp ".;postgresql-42.2.8.jar" Main.java
