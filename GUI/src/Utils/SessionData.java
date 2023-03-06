46e30ed43f6fa9727b4dec518922122298a21733
Mr. Pιηεαρρlξ <Huy Lai>
2023-03-06 11:09:35 -0600

    add comments to SessionData

--- GUI/src/Utils/SessionData.java
+++ GUI/src/Utils/SessionData.java
@@ -1,20 +1,56 @@
 package Utils;
 
-// Bundle of Information that gets passed between scenes
+/**
+ * Bundle of Information that gets passed between scenes
+ * 
+ * @since 2023-03-07
+ * @version 2023-03-07
+ * 
+ * @author Dai, Kevin
+ * @author Davis, Sloan
+ * @author Kuppa Jayaram, Shreeman
+ * @author Lai, Huy
+ * @author Mao, Steven
+ */
 public class SessionData {
-    private DatabaseConnect database;
-    private int employeeId;
+    /**
+     * Connection to the database
+     * 
+     * @see DatabaseConnect
+     */
+    private final DatabaseConnect database;
 
-    SessionData(DatabaseConnect database, int employeeId) {
+    /**
+     * Identification number of the employee
+     */
+    private final int employeeId;
+
+    /**
+     * Constructor
+     * 
+     * @param database Connection to the database
+     * @param employeeId id of the employee
+     */
+    SessionData(final DatabaseConnect database, final int employeeId) {
         this.database = database;
         this.employeeId = employeeId;
     }
 
+    /**
+     * Gets {@link #database}
+     * 
+     * @return {@link #database}
+     */
     public DatabaseConnect getDatabase() {
-        return database;
+        return this.database;
     }
 
+    /**
+     * get {@link #employeeId}
+     * 
+     * @return {@link #employeeId}
+     */
     public int getEmployeeId() {
-        return employeeId;
+        return this.employeeId;
     }
 }
