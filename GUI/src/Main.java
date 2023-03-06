04de328fafd5962505c266a950ad6b2ffc15c9cc
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-06 09:34:20 -0600

    fix controller linking

--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("server.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("./FXML/server.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
