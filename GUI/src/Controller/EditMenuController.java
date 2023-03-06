1f17d0ed9db641c99ce47efa42a2832a72f12d73
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-06 15:25:11 -0600

    change fxml

---
+++ GUI/src/Controller/EditMenuController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.util.ArrayList;
+
+import Utils.SessionData;
+import Utils.DatabaseConnect;
+
+public class EditMenuController {
+    
+}
