bfa4213521b5d6323b94a7d4d2b15a6a1e19ae9a
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-06 09:30:29 -0600

    remove compile script

--- GUI/src/compile.ps1
+++
@@ -1,2 +0,0 @@
-javac *.java --module-path "C:\\Program Files\\Java\\javafx-sdk-19.0.2.1\\lib" --add-modules javafx.controls,javafx.fxml
-java --module-path "C:\\Program Files\\Java\\javafx-sdk-19.0.2.1\\lib" --add-modules javafx.controls,javafx.fxml -cp ".;postgresql-42.2.8.jar" Main.java
