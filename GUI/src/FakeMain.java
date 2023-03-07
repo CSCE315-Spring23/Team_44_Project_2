e9c74e33e4528aad7cbfc5af0a0135a8e3d89b7f
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-07 15:10:44 -0600

    add fake main to show how to pass session data

---
+++ GUI/src/FakeMain.java
@@ -0,0 +1,52 @@
+import Controller.EditMenuController;
+import Items.Order;
+import Utils.DatabaseConnect;
+import Utils.DatabaseLoginInfo;
+import Utils.SessionData;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+/*
+ * 
+ * Hello Everyone, this is a fake main class that shows how we can pass a controller 
+ * that has the session in the constructor. If you get an error REMOVE CONTROLLER FROM FXML.
+ */
+
+public class FakeMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        DatabaseConnect database;
+        String dbConnectionString = DatabaseLoginInfo.dbConnectionString;
+        String username = DatabaseLoginInfo.username;
+        String password = DatabaseLoginInfo.password;
+
+        database = new DatabaseConnect(dbConnectionString, username, password);
+        database.setUpDatabase();
+
+        SessionData session = new SessionData(database, 0, new Order(0));
+
+        EditMenuController menuController = new EditMenuController(session);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("./FXML/EditMenu.fxml"));
+
+        loader.setController(menuController);
+
+        Parent root = loader.load();
+    
+
+        primaryStage.setTitle("Chick-fil-A");
+        primaryStage.getIcons().add(new Image("./resources/logo.png"));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
