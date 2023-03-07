cd2819d57c15643e6bf81dd8bc5ef7c8b42ea90f
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-03-06 22:03:19 -0600

    update menu items

--- GUI/src/Controller/EditMenuController.java
+++ GUI/src/Controller/EditMenuController.java
@@ -1,8 +1,13 @@
 package Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import Utils.SessionData;
@@ -12,6 +17,10 @@ public class EditMenuController {
 
     private DatabaseConnect database;
     private SessionData session;
+    private ArrayList<String> menuItems;
+
+    @FXML
+    private ListView menuList;
 
     public EditMenuController(SessionData session) {
         this.session = session;
@@ -19,11 +28,45 @@ public class EditMenuController {
     }
 
     public void initialize() {
+        ResultSet rs = getMenuItemsQuery();
+
+        try {
+            readMenuItems(rs);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("COULDN'T READ MENU ITEMS");
+            e.printStackTrace();
+        }
+
+        addMenuItemsToListView();
 
     }
 
-    private void getMenuItemsQuery(){
-
+    private ResultSet getMenuItemsQuery(){
+        String query = "SELECT * FROM menuitem";
+        ResultSet rs = database.executeQuery(query);
+        return rs;
     }
 
+    private void readMenuItems(ResultSet rs) throws SQLException{
+        menuItems = new ArrayList<>();
+        if(rs == null){
+            menuItems.add("No Menu Items Retrieved");
+            return;
+        }
+        while(rs.next()){
+            String curLine = "";
+            curLine += "ID: " + Integer.valueOf(rs.getInt("id")).toString() + ", ";
+            curLine += "name: " + rs.getString("name") + ", ";
+            curLine += "cost: " + rs.getString("cost") + ", ";
+            curLine += "numbersold: " + rs.getString("numbersold") + " ";
+            menuItems.add(curLine);
+        }
+    }
+
+    private void addMenuItemsToListView(){
+        ObservableList<String> items = FXCollections.observableArrayList(menuItems);
+        menuList.setItems(items);
+
+    }
 }
