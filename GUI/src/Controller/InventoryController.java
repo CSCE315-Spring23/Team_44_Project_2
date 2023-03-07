5c1a54111511a62845366d78749bad3a90431103
Mr. Pιηεαρρlξ <Huy Lai>
2023-03-06 19:48:23 -0600

    update inventory

--- GUI/src/Controller/InventoryController.java
+++ GUI/src/Controller/InventoryController.java
@@ -2,14 +2,16 @@ package Controller;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.InvalidNameException;
 import Inventory.InventoryItem;
 import Utils.DatabaseConnect;
 import Utils.DatabaseLoginInfo;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
 
 public class InventoryController {
     DatabaseConnect database;
@@ -17,8 +19,17 @@ public class InventoryController {
     @FXML
     private Button logOut;
 
+    // @FXML
+    // private TableView<InventoryItem> inventoryTable;
+
+    // @FXML
+    // private TableColumn<InventoryItem, String> nameColumn;
+
+    // @FXML
+    // private TableColumn<InventoryItem, Long> quantityColumn;
+
     @FXML
-    private TableView<InventoryItem> inventoryTable;
+    private TextArea inventoryTable;
 
     public void initialize() {
         // database login info
@@ -30,27 +41,51 @@ public class InventoryController {
         database.setUpDatabase();
         System.out.println("Database Connection Established");
 
-        ObservableList<InventoryItem> items = FXCollections.observableArrayList();
+        StringBuilder sb = new StringBuilder();
 
         ResultSet set = database.executeQuery("SELECT * FROM inventory");
 
-        String name;
-        long quant;
+        List<InventoryItem> items = new ArrayList<>();
+
         try {
+            // Calculate maximum length of name and quantity fields
+            int maxNameLength = 0;
+            int maxQuantityLength = 0;
             while (set.next()) {
-                name = set.getString("name");
-                quant = set.getLong("quantity");
-                items.add(new InventoryItem(name, quant));
+                String name = set.getString("name");
+                long quantity = set.getLong("quantity");
+                InventoryItem item = new InventoryItem(name, quantity);
+                items.add(item);
+                maxNameLength = Math.max(maxNameLength, item.getName().length());
+                maxQuantityLength =
+                        Math.max(maxQuantityLength, String.valueOf(item.getQuantity()).length());
+            }
+
+            sb.append(String.format("%-" + maxNameLength + "s", "Name"));
+            sb.append(" | ");
+            sb.append(String.format("%" + maxQuantityLength + "s", "Quant\n"));
+
+            for (int i = 0; i < maxNameLength + maxQuantityLength + 3; ++i)
+                sb.append('-');
+            sb.append('\n');
+
+            for (InventoryItem item : items) {
+                String paddedName = String.format("%-" + maxNameLength + "s", item.getName());
+                String paddedQuantity =
+                        String.format("%" + maxQuantityLength + "s", item.getQuantity());
+                String itemString = paddedName + " | " + paddedQuantity + "\n";
+
+                sb.append(itemString);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        for (InventoryItem item : items)
-            System.out.println(item);
-
-        this.inventoryTable.setItems(items);
+        // Set text to TextArea
+        this.inventoryTable.setFont(Font.font("Monospaced")); // Use fixed-width font
+        this.inventoryTable.setText(sb.toString());
     }
+
 
     public void openOrders() {
         System.out.println("Switch to order view");
