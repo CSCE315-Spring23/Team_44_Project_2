58921ff82dd23832b14d51e99b5c9a266036d761
Huy Lai <lai.huy@tamu.edu>
2023-03-08 15:50:58 -0600

    update database names

--- GUI/src/Utils/DatabaseNames.java
+++ GUI/src/Utils/DatabaseNames.java
@@ -1,10 +1,10 @@
 package Utils;
 
 public final class DatabaseNames {
+    public static final String ORDER_ITEM_DATABASE = "orderitem";
+    public static final String SOLD_ITEM_DATABASE = "solditem";
+    public static final String MENU_ITEM_DATABASE = "menuitem";
+    public static final String RECIPE_ITEM_DATABASE = "recipeitem";
     public static final String INVENTORY_DATABASE = "inventory";
-    public static final String ORDER_HIST_DATABASE = "orderhistory";
-    public static final String ORDER_DATABASE = "orderitem";
     public static final String EMPLOYEE_DATABASE = "employee";
-    public static final String MENU_ITEM = "menuitem";
-    public static final String RECIPE_DATABASE = "recipeitem";
 }
