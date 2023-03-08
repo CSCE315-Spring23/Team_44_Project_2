32b2f9207109f96b47a3eeb2814ce85d2dbaefc9
Huy Lai <lai.huy@tamu.edu>
2023-03-08 15:49:20 -0600

    add database names

---
+++ GUI/src/Utils/DatabaseNames.java
@@ -0,0 +1,10 @@
+package Utils;
+
+public final class DatabaseNames {
+    public static final String INVENTORY_DATABASE = "inventory";
+    public static final String ORDER_HIST_DATABASE = "orderhistory";
+    public static final String ORDER_DATABASE = "orderitem";
+    public static final String EMPLOYEE_DATABASE = "employee";
+    public static final String MENU_ITEM = "menuitem";
+    public static final String RECIPE_DATABASE = "recipeitem";
+}
